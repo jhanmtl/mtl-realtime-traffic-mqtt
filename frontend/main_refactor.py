c0dd5456afe68dded753b0b6f1267129b96d8aa5
jhanmtl <jing.han@mail.mcgill.ca>
2021-01-19 13:18:19 -0500

    refactoring scatter class

--- frontend/main_refactor.py
+++ frontend/main_refactor.py
@@ -13,47 +13,45 @@ import numpy as np
 import json
 from layout_utils import CustomBar, CustomTable, CustomScatter, CountdownSpinner
 
+
+# ================= load configs ==============================================
 with open("./assets/bar_config.json", "r") as jfile:
     plot_config = json.load(jfile)
 
 with open("./assets/slider_config.json", "r") as jfile:
     slider_config = json.load(jfile)
 
-df = pd.read_csv("../data/detectors-active.csv")
+n = 20
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-layout, left_column, right_column = layout_utils.init_layout()
-
-map_fig, map_data = frontend_utils.init_map(df)
-right_column.get_subpanel_by_id('map-pane').children.figure = map_fig
-map_fig.update_layout(paper_bgcolor="gray", margin=dict(l=0, r=0, b=0, t=0))
-
+# ================= instantiate db, fetch data =================================
 db = frontend_utils.RedisDB()
-stations = ["station {}".format(i + 1) for i in range(len(df))]
-
 speed_values = db.latest_readings("vehicle-speed")
 count_values = db.latest_readings("vehicle-count")
 gap_values = db.latest_readings("vehicle-gap-time")
 
-summary = {
-    "station": (np.arange(len(stations)) + 1).tolist(),
-    "speed (kmh)": speed_values,
-    "count (cars)": count_values,
-    "gap time (s)": gap_values
-}
+df = pd.read_csv("../data/detectors-active.csv")
+stations = ["station {}".format(i + 1) for i in range(len(df))]
 
-summary = pd.DataFrame.from_dict(summary)
-summary["corner"] = df["corner_st2"]
-summary = summary[["station", "corner", "speed (kmh)", "count (cars)", "gap time (s)"]]
+# ==================== app entry pt ===========================================
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+layout, left_column, right_column = layout_utils.init_layout()
 
 speed_card = left_column.get_subpanel_by_id("vspeed").children
 count_card = left_column.get_subpanel_by_id("vcount").children
 gap_card = left_column.get_subpanel_by_id("vgap").children
-
 refresh_card = right_column.get_subpanel_by_id("stat-3").children
+hist_card = right_column.get_subpanel_by_id("hist-pane").children
 
+
+# ================== map stuff ================================================
+map_fig, map_data = frontend_utils.init_map(df)
+right_column.get_subpanel_by_id('map-pane').children.figure = map_fig
+map_fig.update_layout(paper_bgcolor="gray", margin=dict(l=0, r=0, b=0, t=0))
+
+# ================== countdown spinner =======================================
 spinner = CountdownSpinner(plot_config, "seconds to next update", refresh_card, "pie-graph")
 
+# ================== bar plots ===============================================
 speedbar = CustomBar(plot_config, "speed dectected", speed_card, "speed-live-graph")
 countbar = CustomBar(plot_config, "vehicles counted", count_card, "count-live-graph")
 gapbar = CustomBar(plot_config, "gap time between vehicles", gap_card, "gap-live-graph")
@@ -62,29 +60,20 @@ speedbar.set_data(speed_values, stations, "kmh")
 countbar.set_data(count_values, stations, "cars")
 gapbar.set_data(gap_values, stations, "s")
 
-aux_card = left_column.get_subpanel_by_id("aux").children
-table = CustomTable(plot_config, "detector metrics summary", aux_card)
-table.set_data(summary)
-
-n = 20
-
-hist_card = right_column.get_subpanel_by_id("hist-pane").children
-
+# =================== hist scatter plot =====================================
 hist_speed = db.n_latest_readings("vehicle-speed", n)
 hist_utc = db.n_latest_readings("time", n)[0]
-stations = ["station {}".format(i + 1) for i in range(len(db.keys))]
-hist_speed_dict = {s: l for s, l in zip(stations, hist_speed)}
-
+hist_dict = {s: l for s, l in zip(stations, hist_speed)}
 hplot = CustomScatter(plot_config, "historic data over last 24 hrs - use slider to adjust range", hist_card, stations)
 
-values1 = hist_speed_dict["station 1"]
-values2 = hist_speed_dict["station 2"]
+values1 = hist_dict["station 1"]
+values2 = hist_dict["station 2"]
 
 hplot.set_primary(hist_utc, values1, "kmh")
 hplot.set_seconary(values2)
 
-hplot.update_primary(hist_utc,values1)
-hplot.update_secondary(hist_utc,values2)
+hplot.update_primary(hist_utc, values1)
+hplot.update_secondary(hist_utc, values2)
 
 app.layout = layout
 
@@ -97,7 +86,7 @@ app.layout = layout
               Input("hist-slider", "value"),
               )
 def update_slider_labels(v):
-    global  hist_utc
+    global hist_utc
 
     [value_left, value_right] = v
     # print(v)
@@ -192,10 +181,10 @@ def update_live(n1, n2, n3, n4, value_range, selection_1, selection_2):
 
     windowed_primary = hplot.val_primary[i:j]
     windowed_secondary = hplot.val_secondary[i:j]
-    windowed_time=hplot.time[i:j]
+    windowed_time = hplot.time[i:j]
 
     hplot.update_primary(windowed_time, windowed_primary)
-    hplot.update_secondary(windowed_time,windowed_secondary)
+    hplot.update_secondary(windowed_time, windowed_secondary)
 
     return speedbar.fig, countbar.fig, gapbar.fig, hplot.fig
 
