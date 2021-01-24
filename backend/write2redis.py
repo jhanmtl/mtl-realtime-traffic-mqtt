c916c9e83c9a0d5e5b39d128145fad467343a68a
jhanmtl <jing.han@mail.mcgill.ca>
2021-01-24 16:18:11 -0500

    utility method for write2redis

---
+++ backend/write2redis.py
@@ -0,0 +1,14 @@
+import redis
+import json
+import os
+
+db=redis.Redis()
+files=os.listdir("../data/placeholders")
+jfiles=[i for i in files if ".json" in i and "data" not in i]
+
+for j in jfiles:
+    key=j.replace(".json","")
+    j="../data/placeholders/"+j
+    with open(j,"r") as f:
+        data=json.load(f)
+        db.set(key,json.dumps(data))
